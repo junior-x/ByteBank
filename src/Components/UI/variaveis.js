ad5efb48bd6030f1448a6c6fcd3041eb1a9c4a3d
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-04-08 23:53:00 -0300

    Delete src directory

--- src/Components/UI/variaveis.js
+++
@@ -1,9 +0,0 @@
-export const corPrimaria = "#7E3D90";
-export const fundoClaro = "#f1f1f1";
-export const textoFundoClaro = "grey";
-
-export const conteudoClaro = "white";
-
-export const fundoEscuro ="#241a2b";
-export const conteudoEscuro = "#6e7291";
-export const textoFundoEscuro = "#FAFAFA";
